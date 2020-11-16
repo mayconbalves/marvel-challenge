c595314c3cff2377e09c75fc417289b8ac938086
mayconbalves <mayconmustaine@hotmail.com>
2020-11-16 19:27:10 -0300

    test(home): remove mock tests and create two switchs

--- src/pages/home/Home.test.js
+++ src/pages/home/Home.test.js
@@ -1,8 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import Home from './';
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import Home from './'
 
-test('renders title of home', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/home/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<Home />', () => {
+  const mockStore = configureStore([thunk])
+  let store
+  store = mockStore({
+    charactersReducer: { characters: [] }
+  })
+
+  it('should render correct title element', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    expect(
+      screen.getByRole('heading', { name: /Explore o universo/i })
+    ).toBeInTheDocument()
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render correct input element', () => {
+    const container = render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <Home />
+        </Provider>
+      </MemoryRouter>
+    )
+
+    expect(
+      container.getByPlaceholderText(/Procure por heróis/i)
+    ).toBeInTheDocument()
+  })
+})
