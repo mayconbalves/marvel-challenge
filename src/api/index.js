a5240738e8efb2cd54e63e69184137526d9e565c
mayconbalves <mayconmustaine@hotmail.com>
2020-11-16 18:49:17 -0300

    feat(*): create entry point for all urls

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import md5 from 'js-md5'
+
+const PRIVATE_KEY = '358a7f7e001f105f13dea1b03b938ff2e85f7d0d'
+const PUBLIC_KEY = 'ed7a7b83b6e8f9f675216ce8cd676cc3'
+const url = 'https://gateway.marvel.com/v1/public'
+
+const timestamp = Number(new Date())
+const hash = md5.create()
+hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY)
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  baseUrl: url,
+  CHARACTERS_LIST: `${url}/characters?ts=${timestamp}&orderBy=name&limit=20&apikey=${PUBLIC_KEY}&hash=${hash.hex()}`
+}
