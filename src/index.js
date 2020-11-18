3c2ed645e5d91f9cba498158cd1abd0a7295de19
mayconbalves <mayconmustaine@hotmail.com>
2020-11-17 22:38:32 -0300

    chore(*): add two props in reset css

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    list-style: none;
+    text-decoration: none;
     font-family: Work Sans, Arial, Helvetica, sans-serif;
   }
 `
