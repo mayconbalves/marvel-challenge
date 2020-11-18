9639b924aaf5a475e86addbd86c0186abb1ec657
mayconbalves <mayconmustaine@hotmail.com>
2020-11-17 22:32:10 -0300

    feat(root): create character details router

--- src/Root.js
+++ src/Root.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import Home from 'pages/home'
+import CharacterDetails from 'pages/character-details'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Switch>
         <Route exact path='/' component={Home} />
+        <Route exact path='/characters/:id' component={CharacterDetails} />
+        <Redirect to='/' />
       </Switch>
     </Router>
   </Provider>
